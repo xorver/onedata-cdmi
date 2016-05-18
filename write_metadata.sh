081bb0785b16849ed1462720946e3d37a84d27fb
Tomasz Lichon <xorver@gmail.com>
2016-05-18 21:06:02 +0200

    Add read/write metadata example

---
+++ write_metadata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+source ./cdmi_env
+
+curl -k -v --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H "X-CDMI-Specification-Version: 1.1.1" -H "Accept: application/cdmi-object" -H "Content-Type: application/cdmi-object" \
+-d '{"metadata": {"meta1": "value1", "meta2": "value2"}}' -X PUT $ENDPOINT/file.txt?metadata
