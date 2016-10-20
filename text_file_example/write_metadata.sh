8ac45eeec166ce65986a2d6618b1b0f5ec80aabd
Tomasz Lichon <t.j.lichon@gmail.com>
2016-10-20 17:14:30 +0200

    Switch write_metadata script from cdmi to rest.

--- text_file_example/write_metadata.sh
+++ text_file_example/write_metadata.sh
@@ -7,5 +7,5 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-curl -k -v --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H "X-CDMI-Specification-Version: 1.1.1" -H "Accept: application/cdmi-object" -H "Content-Type: application/cdmi-object" \
--d '{"metadata": {"onedata_json": {"category": "test"}}}' -X PUT "$ENDPOINT/cdmi/$SPACE_NAME/$1?metadata"
+curl -k -v --tlsv1.2 -H 'content-type: application/json' -H "X-Auth-Token: $TOKEN" \
+    -X PUT -d '{"category" : "test"}' "$ENDPOINT/api/v3/oneprovider/metadata/$SPACE_NAME/$1?metadata_type=json"
