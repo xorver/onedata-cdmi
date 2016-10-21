addac55924bf7e6ee0959dad6d2f846cda07a28d
Tomasz Lichon <t.j.lichon@gmail.com>
2016-10-21 08:53:22 +0200

    Add optional argument to 02_metadata_changes_stream.sh for requesting changes starting from given sqe_number.

--- example/02_metadata_changes_stream.sh
+++ example/02_metadata_changes_stream.sh
@@ -2,4 +2,8 @@
 
 . ../cdmi_env
 
-curl -v -k -N --tlsv1.2 -Ss -H "X-Auth-Token: $TOKEN" -X GET "$ENDPOINT/api/v3/oneprovider/changes/metadata/$SPACE_ID"
+if [[ $# -eq 1 ]] ; then
+    SEQUENCE_NUMBER_OPTION="?last_seq=$1"
+fi
+
+curl -v -k -N --tlsv1.2 -Ss -H "X-Auth-Token: $TOKEN" -X GET "$ENDPOINT/api/v3/oneprovider/changes/metadata/$SPACE_ID$SEQUENCE_NUMBER_OPTION"
