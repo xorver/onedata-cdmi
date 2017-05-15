427da86c77987af0ecdb27c792f43878eed3b34b
Tomasz Lichon <t.j.lichon@gmail.com>
2017-05-15 14:14:50 +0200

    Add missing cdmi version header to mkdir, change index content type from text/javascript to applization/javascipt.

--- example/00_upload_files.sh
+++ example/00_upload_files.sh
@@ -2,7 +2,7 @@
 
 . ../cdmi_env
 
-curl -v -k --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H 'Accept: */*' -H 'Content-type: application/cdmi-container' \
+curl -v -k --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H 'Accept: */*' -H 'Content-type: application/cdmi-container' -H 'X-CDMI-Specification-Version: 1.1.1' \
     -X PUT "$ENDPOINT/cdmi/$SPACE_NAME/test_files/"
 curl -v -k --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H 'Accept: */*' -T ./test_files/file1.txt \
     -X PUT "$ENDPOINT/cdmi/$SPACE_NAME/test_files/file1.txt"
--- example/05_create_index.sh
+++ example/05_create_index.sh
@@ -7,5 +7,5 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-curl -v -k --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H 'content-type: text/javascript' \
+curl -v -k --tlsv1.2 -H "X-Auth-Token: $TOKEN" -H 'content-type: application/javascript' \
     -X POST -d '@./index.js' "$ENDPOINT/api/v3/oneprovider/index?space_id=$SPACE_ID&name=$1"
